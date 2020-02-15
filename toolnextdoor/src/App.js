bd5ebef2ad8cdba0982fa0fc6764a17d8a2587fb
baohong1998 <bao_mu2012@yahoo.com.vn>
2020-02-15 15:12:59 -0500

    login page

--- toolnextdoor/src/App.js
+++ toolnextdoor/src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/login/login";
 import Header from "./components/headers/header";
+import MainAppContainer from "./pages/mainApp/mainAppContainer";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router className="App">
       <Header />
       <Switch>
+        <Route path="/app">
+          <MainAppContainer />
+        </Route>
         <Route path="/">
           <LoginPage />
         </Route>
--- toolnextdoor/src/pages/login/login.js
+++ toolnextdoor/src/pages/login/login.js
@@ -1,21 +1,23 @@
 import React from "react";
-
+import "./login.css";
+import { Link } from "react-router-dom";
 export default function LoginPage() {
   return (
-    <div className>
+    <div className="form-container">
+      <div className="form-group form-head">
+        <p className="big-text">Welcome to Tool Next Door</p>
+        <p>Where you can find your need immediately in your neighborhood</p>
+      </div>
       <div className="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="exampleInputEmail1">Username</label>
         <input
           type="email"
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
         />
-        <small id="emailHelp" class="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="exampleInputPassword1">Password</label>
         <input
           type="password"
@@ -23,15 +25,12 @@ export default function LoginPage() {
           id="exampleInputPassword1"
         />
       </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div>
-      <button type="submit" class="btn btn-primary">
-        Submit
-      </button>
+      <p>Create an account</p>
+      <Link to="/app">
+        <button type="submit" class="btn btn-primary">
+          Login
+        </button>
+      </Link>
     </div>
   );
 }
