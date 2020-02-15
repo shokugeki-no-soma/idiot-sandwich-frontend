c3b286ae7a71803699a3c3d085e1ca3b7120072d
baohong1998 <bao_mu2012@yahoo.com.vn>
2020-02-15 14:32:08 -0500

    init bootstrap

---
+++ toolnextdoor/src/components/headers/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./header.css";
+export default function Header() {
+  return (
+    <div className="header">
+      <p className="header-text">Tool Next Door</p>
+    </div>
+  );
+}
